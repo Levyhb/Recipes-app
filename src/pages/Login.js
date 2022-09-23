11f487384bc41208f988237e795d0b6b95c89c63
Danielle Gazarini <daniellegazarini@gmail.com>
2022-09-23 20:29:25 -0300

    Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-
+// LOgin
 export default function Login({ history }) {
   const [user, setUser] = useState({ email: '', password: '' });
   const [isBtnDisabled, setIsBtnDisabled] = useState(true);
