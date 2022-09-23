ad7cb3816f3645758bc39312d4023aa39bbf04be
Danielle Gazarini <daniellegazarini@gmail.com>
2022-09-23 19:17:29 -0300

    Requisito 3

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Login() {
+  const [user, setUser] = useState({ email: '', password: '' });
+
+  const handleInput = ({ target }) => {
+    setUser({ ...user, [target.name]: target.value });
+  };
+
   return (
     <div>
       <input
         data-testid="email-input"
         type="text"
+        name="email"
+        value={ user.email }
+        onChange={ handleInput }
       />
       <input
         data-testid="password-input"
         type="password"
+        name="password"
+        value={ user.password }
+        onChange={ handleInput }
       />
       <button
         data-testid="login-submit-btn"
