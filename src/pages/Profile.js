54e73a832fb1e9613b0135a38b485894d164eb68
DiegoLiander <diego_liander@hotmail.com>
2022-09-26 14:49:29 -0300

    req 57-62

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-export default function Profile() {
+function Profile() {
+  const user = JSON.parse(localStorage.getItem('user'));
   return (
-    <div>
-      <Header title="Profile" profileIcon />
+    <>
+      <div>
+        <Header />
+        <div>
+          <p data-testid="profile-email">{ user !== null ? user.email : 'email' }</p>
+        </div>
+        <div>
+          <Link to="/done-recipes">
+            <button
+              type="button"
+              data-testid="profile-done-btn"
+            >
+              Done Recipes
+            </button>
+          </Link>
+          <Link to="/favorite-recipes">
+            <button
+              type="button"
+              data-testid="profile-favorite-btn"
+            >
+              Favorite Recipes
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              onClick={ () => localStorage.clear() }
+              type="button"
+              data-testid="profile-logout-btn"
+            >
+              Logout
+            </button>
+          </Link>
+        </div>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
+
+export default Profile;
