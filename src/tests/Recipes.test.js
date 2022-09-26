56b904a858cf6b98b686b6a97a5e90f8f9095f4a
Danielle Gazarini <daniellegazarini@gmail.com>
2022-09-26 17:06:34 -0300

    Requisito 23 sem testes

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import renderWithRouterAndRedux from './helpers/renderWith';
-import Recipes from '../pages/Recipes';
-import mealsMock from './helpers/mealsMock';
+// import React from 'react';
+// import renderWithRouterAndRedux from './helpers/renderWith';
+// import Recipes from '../pages/Recipes';
+// import mealsMock from './helpers/mealsMock';
 
-beforeEach(() => {
-  global.fetch = jest.fn(async () => ({
-    json: async () => mealsMock,
-  }));
-});
+// // beforeEach(() => {
+// //   global.fetch = jest.fn(async () => ({
+// //     json: async () => mealsMock,
+// //   }));
+// // });
 
-describe('Testa renderização da página Recipes', () => {
-  it('verifica o caminho da página', () => {
-    renderWithRouterAndRedux(<Recipes />);
-  });
-});
+// const INITIAL_STATE = {
+//   user: { email: 'BLA '},
+//   meals: { MEALS: [] },
+//   drinks: { drinks: [] },
+// }
+
+// describe('Testa renderização da página Recipes', () => {
+//   it('verifica o caminho da página', () => {
+//     const { store } = renderWithRouterAndRedux(<Recipes />, { initialState: INITIAL_STATE });
+
+//     console.log(store.getState())
+//   });
+// });
