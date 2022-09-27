c3e7a6c79b74b226242a932c3bf0de6964e399d7
Danielle Gazarini <daniellegazarini@gmail.com>
2022-09-27 15:21:39 -0300

    abrindo PR

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,7 @@
 export const MEAL_DETAIL = 'MEAL_DETAIL';
 export const DRINK_DETAIL = 'DRINK_DETAIL';
 export const FOODS_RECIPES = 'FOODS_RECIPES';
-// work
+
 export const getMealDetail = (payload) => ({ type: MEAL_DETAIL, payload });
 export const getDrinkDetail = (payload) => ({ type: DRINK_DETAIL, payload });
 export const foodsRecipes = (payload) => ({
