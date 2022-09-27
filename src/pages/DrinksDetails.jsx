9ae7061e76ae98d357788f4a12ab69bcc4306fb9
Levy Bezerra <levybholanda@gmail.com>
2022-09-27 19:24:34 -0300

    Requisito 26 e 27

--- src/pages/DrinksDetails.jsx
+++ src/pages/DrinksDetails.jsx
@@ -1,16 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
+import '../styles/components/Carrossel.css';
+import CarouselCard from '../components/CarouselCard';
+import ArrowCarousel from '../components/ArrowCarousel';
+
+const mealsMaxLength = 6;
 
 export default function DrinksDetails() {
   const [drinkDetails, setDrinkDetails] = useState();
+  const [relatedFoods, setRelatedFoods] = useState();
+  const carouselMealsRef = useRef(null);
+
   const { id } = useParams();
   useEffect(() => {
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
       .then((response) => response.json())
       .then((data) => setDrinkDetails(data));
     console.log(drinkDetails);
+
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
+      .then((response) => response.json())
+      .then((data) => setRelatedFoods(data.meals.slice(0, mealsMaxLength)));
+    console.log(relatedFoods);
   }, []);
+
   return (
-    <div>DrinksDetails</div>
+    <div>
+      DrinksDetails
+      <div className="details-carousel">
+        {!relatedFoods ? (
+          <p>Carregando...</p>
+        ) : (
+          <div
+            className="carousel-container"
+            ref={ carouselMealsRef }
+          >
+            {relatedFoods.map((food, index) => (
+              <CarouselCard
+                title={ food.strMeal }
+                thumb={ food.strMealThumb }
+                index={ index }
+                key={ food.idMeal }
+              />
+            ))}
+          </div>
+        )}
+        <ArrowCarousel carousel={ carouselMealsRef } />
+      </div>
+    </div>
   );
 }
--- src/pages/RecipesDetails.jsx
+++ src/pages/RecipesDetails.jsx
@@ -1,17 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
+import ArrowCarousel from '../components/ArrowCarousel';
+import CarouselCard from '../components/CarouselCard';
+import '../styles/components/Carrossel.css';
+
+const maxDrinksLength = 6;
 
 export default function RecipesDetails() {
   const [mealDetail, setMealDetail] = useState();
+  const [recommendedDrinks, setRecommendedDrinks] = useState();
+  const carouselDrinksRef = useRef(null);
+
   const { id } = useParams();
   useEffect(() => {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
       .then((response) => response.json())
       .then((data) => setMealDetail(data));
     console.log(mealDetail);
+
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
+      .then((response) => response.json())
+      .then((data) => setRecommendedDrinks(data.drinks.slice(0, maxDrinksLength)));
   }, []);
 
   return (
-    <div>RecipesDetails</div>
+    <div>
+      RecipeDetails
+      <div className="details-carousel">
+        {!recommendedDrinks ? (
+          <p>Carregando...</p>
+        ) : (
+          <div className="carousel-container" ref={ carouselDrinksRef }>
+            {recommendedDrinks.map((drink, index) => (
+              <CarouselCard
+                title={ drink.strDrink }
+                thumb={ drink.strDrinkThumb }
+                index={ index }
+                key={ drink.idDrink }
+              />
+            ))}
+          </div>
+        )}
+        <ArrowCarousel carousel={ carouselDrinksRef } />
+      </div>
+    </div>
   );
 }
