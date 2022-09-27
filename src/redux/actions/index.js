96c6f7045eb14a53d16accad54c4f0f16f01e4c9
Danielle Gazarini <daniellegazarini@gmail.com>
2022-09-27 15:14:19 -0300

    atualização pr

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,16 +1,10 @@
-
 export const MEAL_DETAIL = 'MEAL_DETAIL';
 export const DRINK_DETAIL = 'DRINK_DETAIL';
+export const FOODS_RECIPES = 'FOODS_RECIPES';
 
 export const getMealDetail = (payload) => ({ type: MEAL_DETAIL, payload });
 export const getDrinkDetail = (payload) => ({ type: DRINK_DETAIL, payload });
-
-export const FOODS_RECIPES = 'FOODS_RECIPES';
-
-const foodsRecipes = (payload) => ({
+export const foodsRecipes = (payload) => ({
   type: FOODS_RECIPES,
   payload,
 });
-
-export default foodsRecipes;
-
