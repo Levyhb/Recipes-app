bc7a0a7d2127ca878a5c0dfcecefd9634badee8f
Danielle Gazarini <daniellegazarini@gmail.com>
2022-10-03 14:14:11 -0300

    erros de lint corrigidos

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,23 +1,10 @@
-import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import '../styles/components/Carrossel.css';
-import { useSelector } from 'react-redux';
 import MealInProgess from '../components/MealInProgess';
 import DrinkInProgress from '../components/DrinkInProgress';
 
 export default function RecipeInProgress() {
   const location = useLocation();
-  // const key = { drinks: {}, meals: {} };
-  // const storageState = useSelector((state) => state.conditionalRecipe.recipesInProgress);
-  // const storage = localStorage.getItem('inProgressRecipes')
-  // console.log(storage)
-  // if (storage === null) {
-  //   localStorage.setItem('inProgressRecipes', JSON.stringify(key))
-  // }
-  // useEffect(() => {
-  //   // const key = { drinks: {}, meals: {} };
-  //   // localStorage.setItem('inProgressRecipes', JSON.stringify(key));
-  // }, []);
   return (
     <div>
       {
